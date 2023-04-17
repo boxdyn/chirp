bafb576705b492a0d9bc03d2a0bebeb18433763a
John Breaux <j@soft.fish>
2023-04-17 05:04:23 -0500

    chip8-test-suite: Update to 4.0

--- tests/chip8_test_suite.rs
+++ tests/chip8_test_suite.rs
@@ -7,7 +7,7 @@ fn setup_environment() -> (CPU, Bus) {
     cpu.flags = Flags {
         debug: true,
         pause: false,
-        monotonic: Some(8),
+        monotonic: Some(10),
         ..Default::default()
     };
     (
@@ -16,28 +16,30 @@ fn setup_environment() -> (CPU, Bus) {
             // Load the charset into ROM
             Charset [0x0050..0x00A0] = include_bytes!("../src/mem/charset.bin"),
             // Load the ROM file into RAM
-            Program [0x0200..0x1000] = include_bytes!("../chip8-test-suite/bin/chip8-test-suite.ch8"),
-            // Create a screen, and fill it with
+            Program [0x0200..0x1000],
+            // Create a screen, and fill it with garbage
             Screen  [0x0F00..0x1000] = include_bytes!("chip8_test_suite.rs"),
         },
     )
 }
 
 struct SuiteTest {
-    test: u16,
+    test: u8,
+    data: &'static [u8],
     screen: &'static [u8],
 }
 
 fn run_screentest(test: SuiteTest, mut cpu: CPU, mut bus: Bus) {
     // Set the test to run
-    bus.write(0x1feu16, test.test);
+    bus.write(0x1ffu16, test.test);
+    bus.load_region(Program, test.data).unwrap();
     // The test suite always initiates a keypause on test completion
-    while !cpu.flags.keypause {
-        cpu.multistep(&mut bus, 8).unwrap();
+    while !(cpu.flags.keypause || cpu.flags.pause) {
+        cpu.multistep(&mut bus, 100).unwrap();
     }
     // Compare the screen to the reference screen buffer
     bus.print_screen().unwrap();
-    bus! {crate::bus::Region::Screen [0..256] = test.screen}
+    bus! {crate::cpu::bus::Region::Screen [0..256] = test.screen}
         .print_screen()
         .unwrap();
     assert_eq!(bus.get_region(Screen).unwrap(), test.screen);
@@ -49,6 +51,7 @@ fn splash_screen() {
     run_screentest(
         SuiteTest {
             test: 0,
+            data: include_bytes!("../chip8-test-suite/bin/1-chip8-logo.ch8"),
             screen: include_bytes!("screens/chip8-test-suite/splash.bin"),
         },
         cpu,
@@ -61,8 +64,23 @@ fn ibm_logo() {
     let (cpu, bus) = setup_environment();
     run_screentest(
         SuiteTest {
-            test: 0x01,
+            test: 0x00,
+            data: include_bytes!("../chip8-test-suite/bin/2-ibm-logo.ch8"),
             screen: include_bytes!("screens/chip8-test-suite/IBM.bin"),
+        },
+        cpu,
+        bus,
+    )
+}
+
+#[test]
+fn corax_test() {
+    let (cpu, bus) = setup_environment();
+    run_screentest(
+        SuiteTest {
+            test: 0x00,
+            data: include_bytes!("../chip8-test-suite/bin/3-corax+.ch8"),
+            screen: include_bytes!("screens/chip8-test-suite/corax+.bin"),
         },
         cpu,
         bus,
@@ -74,7 +92,8 @@ fn flags_test() {
     let (cpu, bus) = setup_environment();
     run_screentest(
         SuiteTest {
-            test: 0x03,
+            test: 0x00,
+            data: include_bytes!("../chip8-test-suite/bin/4-flags.ch8"),
             screen: include_bytes!("screens/chip8-test-suite/flags.bin"),
         },
         cpu,
@@ -87,7 +106,8 @@ fn quirks_test() {
     let (cpu, bus) = setup_environment();
     run_screentest(
         SuiteTest {
-            test: 0x0104,
+            test: 0x01,
+            data: include_bytes!("../chip8-test-suite/bin/5-quirks.ch8"),
             screen: include_bytes!("screens/chip8-test-suite/quirks.bin"),
         },
         cpu,
